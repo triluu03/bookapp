45548b3761035d2a2c1db15a853e71f896912d97
Tri D Luu <91616223+triluu03@users.noreply.github.com>
2022-08-13 10:24:11 +0300

    adding the main components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,39 @@
-function App() {
-    return <div className='App'>This is for book app</div>
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
+
+import Home from './components/Home'
+import BookList from './components/BookList'
+import LoginForm from './components/LoginForm'
+import Credits from './components/Credits'
+
+const App = () => {
+    const style = {
+        padding: 5,
+    }
+
+    return (
+        <BrowserRouter className='app'>
+            <div>
+                <Link style={style} to='/'>
+                    Home
+                </Link>
+                <Link style={style} to='/books'>
+                    Books
+                </Link>
+                <Link style={style} to='/login'>
+                    Login
+                </Link>
+                <Link style={style} to='/credits'>
+                    Credits
+                </Link>
+            </div>
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/books' element={<BookList />} />
+                <Route path='/login' element={<LoginForm />} />
+                <Route path='/credits' element={<Credits />} />
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
 export default App
---
+++ frontend/src/components/LoginForm.js
@@ -0,0 +1,5 @@
+const LoginForm = () => {
+    return <div>Login</div>
+}
+
+export default LoginForm
