9fa82cd7719034ef4d9e138c5a1f2875e7fe10e0
Tri D Luu <91616223+triluu03@users.noreply.github.com>
2022-08-30 13:17:15 +0300

    search book function

--- frontend/src/components/BookList.js
+++ frontend/src/components/BookList.js
@@ -8,7 +8,9 @@ import {
     TableBody,
     Typography,
     Button,
+    TextField,
 } from '@mui/material'
+import { Box } from '@mui/system'
 import ThumbUpIcon from '@mui/icons-material/ThumbUp'
 import ThumbDownIcon from '@mui/icons-material/ThumbDown'
 
@@ -22,17 +24,36 @@ import { Link } from 'react-router-dom'
 
 const BookList = () => {
     const [showBookForm, setShowBookForm] = useState(false)
+    const [bookSearch, setBookSearch] = useState('')
 
     const books = useSelector((state) => state.books)
     const loggedUser = useSelector((state) => state.loggedUser)
 
     const dispatch = useDispatch()
 
+    const booksToShow =
+        bookSearch === ''
+            ? books
+            : books.filter((book) =>
+                  book.name.toLowerCase().includes(bookSearch.toLowerCase())
+              )
+
+    console.log(bookSearch)
+
     return (
         <div>
-            <Typography variant='h4' component='div' sx={{ mt: 3 }}>
-                Books List
-            </Typography>
+            <Box>
+                <Typography variant='h4' component='div' sx={{ mt: 3 }}>
+                    Books List
+                </Typography>
+                <TextField
+                    sx={{ mt: 1 }}
+                    size='small'
+                    placeholder='find a book'
+                    value={bookSearch}
+                    onChange={({ target }) => setBookSearch(target.value)}
+                />
+            </Box>
             <TableContainer component={Paper} variant='contained'>
                 <Table aria-label='books list'>
                     <TableHead>
@@ -50,7 +71,7 @@ const BookList = () => {
                         </TableRow>
                     </TableHead>
                     <TableBody>
-                        {books.map((book) => (
+                        {booksToShow.map((book) => (
                             <TableRow key={book.id}>
                                 <TableCell component='th' scope='row'>
                                     <Link
