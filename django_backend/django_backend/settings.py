a233ff586232e30d544aba349d633420aa90bf76
Tri D Luu <91616223+triluu03@users.noreply.github.com>
2022-12-01 09:43:20 +0200

    Setup Connection to Postgres Database

--- django_backend/django_backend/settings.py
+++ django_backend/django_backend/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = "django_backend.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        'NAME': 'bookapp',
+        'USER': 'postgres',
+        'PASSWORD': '182003',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -105,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "EET"
 
 USE_I18N = True
 
